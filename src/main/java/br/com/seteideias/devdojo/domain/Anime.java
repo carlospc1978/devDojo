59d74c9f2f760389cb2428959da2b6d74f7b9e32
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2022-05-02 15:23:02 -0300

    Initial commit

---
+++ src/main/java/br/com/seteideias/devdojo/domain/Anime.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.devdojo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Anime {
+
+    private Long id;
+    private String name;
+
+}
