6a0cf111b42fde84e8aead31d67a972128758474
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2022-05-22 20:50:49 -0300

    add Interceptador de chamadas web

--- src/main/java/br/com/seteideias/devdojo/controller/AnimeController.java
+++ src/main/java/br/com/seteideias/devdojo/controller/AnimeController.java
@@ -5,6 +5,7 @@ import br.com.seteideias.devdojo.service.AnimeService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
